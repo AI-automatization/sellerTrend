83c4848937ae4a4b23899abf4e5b8fa2486c662f
Sardor Madaliev <sardor@email.com>
2026-02-25 00:30:50 +0500

    fix(api): load swagger-ui assets from CDN for production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -34,7 +34,14 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api/docs', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js',
+    ],
+  });
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
